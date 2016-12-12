abfea6a2a139fbb6371dd86298b173c51587ae0a
rconway2 <rconway2@u.rochester.edu>
2016-12-12 15:51:32 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>RC PORTFOLIO</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://unpkg.com/purecss@0.6.0/build/pure-min.css">
+	<link rel="stylesheet" href="https://unpkg.com/purecss@0.6.0/build/grids-min.css">
+	<link rel="stylesheet" href="https://unpkg.com/purecss@0.6.0/build/grids-responsive-min.css">
+	<link rel="stylesheet" href="content/css/main.css">
+</head>
+<body>
+
+<!-- NAVBAR -->
+<?php include "navbar.php";?>
+
+<!-- LANDING PAGE -->
+<div class="pure-g landingpage">
+	   <div class="pure-u-1 pure-u-md-2-5 pure-u-lg-1-4">
+    	<div class="l-box" id="intro">
+    		<h1>RUAIRI CONWAY</h1>
+  			</br>
+    		<a href="."><h2>PHOTO  |  VIDEO</h2></a>
+    	</div>
+    </div>
+    <div class="pure-u-1 pure-u-md-3-5 pure-u-lg-3-4">
+    	<img class="pure-img" src="content/images/landing.jpg" alt="surfing cover photo">
+    </div>
+</div>
+
+<!-- SECTION 1 -->
+<div class="pure-g content">
+	<div class="pure-u-1 pure-u-md-1-3 pure-u-lg-1-3">
+		<div class="l-box content_textbox">
+			<h2>EDUCATION</h2>
+			<h3>UNIVERSITY OF ROCHESTER, NY</h3>
+			<h4>BA DIGITAL MEDIA | JOURNALISM MINOR</br>Class of 2017</h4>
+			<p>Exploring intersts in:</p>
+			<ul>
+				<li>International Relations</li>
+				<li>Brain and Cognitive Sciences</li>
+			</ul>
+		</div>
+	</div>
+
+	<div class="pure-u-1 pure-u-md-1-3 pure-u-lg-1-3">
+		<div class="l-box content_textbox">
+			<h2>EXHIBITS</h2>
+			<h3>SAGE ART GALLERY</h3>
+			<p>Featured on several occasions for work in </br> digital photography.</p>
+			<h3>RETTNER HALL</h3>
+			<p>Screening of short film.
+			</br>
+			selection based on editing and technique.</p> 
+		</div>
+	</div>
+
+	<div class="pure-u-1 pure-u-md-1-3 pure-u-lg-1-3">
+		<div class="l-box content_textbox">
+			<h2>AWARDS</h2>
+			<h3>ACADEMICS</h3>
+			<p><b>DEANS LIST</b> | From fall 2014 to spring 2016</p>
+			<p><b>JESSE L ROSENBERGER AWARD</b> | fall 2016</p>
+		</div>
+	</div>
+
+    <div class="pure-u-1 pure-u-md-2-3 pure-u-lg-2-3">
+    	<img class="pure-img" src="content/images/personalpic.jpg" alt="surfing cover photo">
+    </div>
+
+    <div class="pure-u-1 pure-u-md-1-3 pure-u-lg-1-3">
+		<div class="l-box content_textbox">
+			<h2>EXPERIENCE</h2>
+</div>
+
+</body>
+</html>
---
+++ navbar.php
@@ -0,0 +1,18 @@
+<div class="custom-wrapper pure-g" id="menu">
+    <div class="pure-u-1 pure-u-md-1-2">
+        <div class="pure-menu">
+            <a href="#" class="pure-menu-heading custom-brand"><b>RC MEDIA</b></a>
+            <a href="#" class="custom-toggle" id="toggle"><s class="bar"></s><s class="bar"></s></a>
+        </div>
+    </div>
+    <div class="pure-u-1 pure-u-md-1-2">
+        <div class="pure-menu pure-menu-horizontal custom-menu-3 custom-can-transform">
+            <ul class="pure-menu-list">
+                <li class="pure-menu-item"><a href="#" class="pure-menu-link"><b>PORTFOLIO</b></a></li>
+                <li class="pure-menu-item"><a href="#" class="pure-menu-link"><b>CONTACT</b></a></li>
+            </ul>
+        </div>
+    </div>
+</div>
+
+<script src="content/js/navbarscript.js"></script>
