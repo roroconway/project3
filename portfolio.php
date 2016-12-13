4d001158a588da6c02bd1dd83903bfbf5c92f9a2
roroconway <ruairiconway@dhcp-10-5-11-35.wireless.rochester.edu>
2016-12-12 23:51:33 -0500

    developed basic HTML structure for section of portfolio
    This section can be copied and pasted over and filled with different
    text and files. CSS classes have been added

--- portfolio.php
+++ portfolio.php
@@ -20,26 +20,47 @@
 <?php include "navbar.php";?>
 
 <div class="pure-g portfolio_title">
-	<div class="pure-u-1 pure-u-md-1-3 pure-u-lg-1-3">
+	<div class="pure-u-1 pure-u-md-1 pure-u-lg-1">
     	<div class="l-box">
-    		<h1>PORTFOLIO</h1>
+    		<h1>PHOTOGRAPHY PORTFOLIO</h1>
     	</div>
     </div>
 </div>
 
 <!-- SECTIONS -->
-<div class="pure-g portfolio">
-	<div class="pure-u-1 pure-u-md-1 pure-u-lg-1">
+<div class="pure-g port_section" id="bw_section">
+
+	<div class="pure-u-0 pure-u-md-1-5 pure-u-lg-1-5">
+    </div>
+
+    <div class="pure-u-1 pure-u-md-1-5 pure-u-lg-1-5">
     	<div class="l-box">
-    		<h2 id="port_title">PHOTOGRAPHY</h2>
+    		<a class="port_heading" href="https://ruairiconway.myportfolio.com/b-w" target="_blank">
+    			<h3>BLACK &amp; WHITE</h3>
+    		</a>
     	</div>
     </div>
-    <div class="pure-u-1 pure-u-md-1 pure-u-lg-1">
-    	<div class="l-box">
-    		<h3>BLACK &amp; WHITE</h3>
+
+    <div class="pure-u-0 pure-u-md-3-5 pure-u-lg-3-5">
     </div>
 
+    <div class="pure-u-0 pure-u-md-1-5 pure-u-lg-1-5">
+    </div>
 
+    <div class="pure-u-1 pure-u-md-1-5 pure-u-lg-1-5">
+    	<a href="https://ruairiconway.myportfolio.com/b-w" target="_blank">
+    		<img class="pure-img" src="https://mir-s3-cdn-cf.behance.net/project_modules/fs/e45ed645858945.583f4ddfef797.jpg" alt="Black and White thumbnail" >
+    	</a>
+    </div>
+
+    <div class="pure-u-1 pure-u-md-2-5 pure-u-lg-2-5">
+    	<div class="l-box">
+    		<p class="port_description"><b>I WAS ON A ROADTRIP</b> with friends from home and I caught some snaps along the way. I used a GoPro for most of the shots and it worked out pretty well. Ireland is always a little cloudy which means proper lighting is hard to come by. So I did what every good photographer does when the photos are a little fuzzy; turned them black and white.</p>
+    	</div>
+    </div>
+
+    <div class="pure-u-0 pure-u-md-1-5 pure-u-lg-1-5">
+    </div>
 </div>
 
 </body>
